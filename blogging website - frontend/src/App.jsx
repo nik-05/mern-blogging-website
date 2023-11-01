2d711d77fdf8c55fbc5b13f7d7ddb0c69e230452
Nikhil <nik.05.jindal@gmail.com>
2023-11-01 21:34:03 +0530

    Navbar and setting up routes

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,6 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar.component";
+
 const App = () => {
     return (
-        <h1>MERN Blogging website by modern web</h1>
+        <Routes>
+            <Route path='/' element={<Navbar/>}>
+            <Route path='signin' element={<h1>Sign In</h1>}/>
+            <Route path='signup' element={<h1>Sign Up</h1>}/>
+            </Route>
+        </Routes>
     )
 }
 
