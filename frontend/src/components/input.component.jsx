4d59ac0bf5be1961d2316942767a99ababe0e8f6
Nikhil <nik.05.jindal@gmail.com>
2023-11-01 23:42:55 +0530

    User Auth Form UI

--- frontend/src/components/input.component.jsx
+++ frontend/src/components/input.component.jsx
@@ -1,6 +1,34 @@
-const InputBox = () => {
+import {useState} from "react";
+
+const InputBox = ({
+    name,
+    type,
+    id,
+    value,
+    placeholder,
+    icon
+}) => {
+
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
     return (
-        <h1>Input Box</h1>
+        <div className="relative w-[100%] mb-4">
+            <input name={name}
+                type={type == 'password' ? passwordVisible ? 'text' : 'password' : type}
+                placeholder={placeholder}
+                defaultValue={value}
+                id={id}
+                className="input-box"/>
+            <i className={
+                "fi " + icon + " input-icon"
+            }></i>
+
+            {
+            type == 'password' ? <i className={"fi fi-rr-eye"+ (!passwordVisible ? '-crossed' : '') +" input-icon left-[auto] right-4 cursor-pointer"}
+                onClick={
+                    () => setPasswordVisible(currentValue => !currentValue)
+            }></i> : ''
+        } </div>
     );
 }
 
