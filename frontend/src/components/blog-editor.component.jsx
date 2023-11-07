29caa46b3a34089b58aba244864178bc9aface7c
Nikhil <nik.05.jindal@gmail.com>
2023-11-07 21:18:04 +0530

    Text editor context & Changing editor state

--- frontend/src/components/blog-editor.component.jsx
+++ frontend/src/components/blog-editor.component.jsx
@@ -14,16 +14,21 @@ const BlogEditor = () => {
     blog,
     blog: { title, banner, content, tags, description },
     setBlog,
+    textEditor,
+    setTextEditor,
+    setEditorState,
   } = useContext(EditorContext);
 
   // use effect
   useEffect(() => {
-    let editor = new EditorJS({
-      holderId: "textEditor",
-      data: "",
-      tools: tools,
-      placeholder: "Let's write an awesome story",
-    });
+    setTextEditor(
+      new EditorJS({
+        holderId: "textEditor",
+        data: "",
+        tools: tools,
+        placeholder: "Let's write an awesome story",
+      })
+    );
   }, []);
 
   const handleBannerUpload = (e) => {
@@ -66,6 +71,32 @@ const BlogEditor = () => {
     image.src = defaultBanner;
   };
 
+  const handlePublishEvent = (e) => {
+    if (!banner.length) {
+      return toast.error("Upload a blog banner to publish it");
+    }
+
+    if (!title.length) {
+      return toast.error("Write blog title to publish it");
+    }
+
+    if (textEditor.isReady) {
+      textEditor
+        .save()
+        .then((data) => {
+          if (data.blocks.length) {
+            setBlog({ ...blog, content: data });
+            setEditorState("publish");
+          } else {
+            return toast.error("Write something in your blog to publish it");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
   return (
     <>
       <nav className="navbar">
@@ -77,7 +108,9 @@ const BlogEditor = () => {
         </p>
 
         <div className="flex gap-4 ml-auto">
-          <button className="btn-dark py-2">Publish</button>
+          <button className="btn-dark py-2" onClick={handlePublishEvent}>
+            Publish
+          </button>
           <button className="btn-light py-2">Save Draft</button>
         </div>
       </nav>
