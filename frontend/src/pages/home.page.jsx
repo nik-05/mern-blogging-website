c05f8d2ef6bcc7efe592376a6ae3c982f16bdb6d
Nikhil <nik.05.jindal@gmail.com>
2023-11-09 09:46:17 +0530

    HomePage fetching latest blogs

--- frontend/src/pages/home.page.jsx
+++ frontend/src/pages/home.page.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
 import AnimationWrapper from "../common/page-animation";
 import InPageNavigation from "../components/inpage-navigation.component";
-
+import axios from "axios";
+import Loader from "../components/loader.component";
 const HomePage = () => {
+  let [blogs, setBlog] = useState(null);
+
+  const fetchLatestBlogs = () => {
+    axios
+      .get(import.meta.env.VITE_SERVER_DOMAIN + "/latest-blogs")
+      .then(({ data }) => {
+        setBlog(data.blogs);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(fetchLatestBlogs, []);
+
   return (
     <AnimationWrapper>
       <section className="h-cover flex justify-center gap-10 ">
@@ -11,7 +28,15 @@ const HomePage = () => {
             routes={["home", "trending blogs"]}
             defaultHidden={["trending blogs"]}
           >
-            <h1>Latest blogs here</h1>
+            <>
+              {blogs == null ? (
+                <Loader />
+              ) : (
+                blogs.map((blog, i) => {
+                  return <h1 key={i}>{blog.title}</h1>;
+                })
+              )}
+            </>
             <h1>Trending blogs here</h1>
           </InPageNavigation>
         </div>
